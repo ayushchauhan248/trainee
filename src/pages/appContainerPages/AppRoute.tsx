fccbbd5321644c3e48363101f4f01ba3fa8a5494
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2022-01-31 17:44:14 +0530

    sidebar added

--- src/pages/appContainerPages/AppRoute.tsx
+++ src/pages/appContainerPages/AppRoute.tsx
@@ -1,12 +1,14 @@
 import { FC, memo } from "react";
 import { Route } from "react-router-dom";
+import Sidebar from "../../components/Sidebar";
 import DashboardPage from "./DashboardPage";
 import RecordingPage from "./RecordingPage";
 interface Props {
 }
 const AppRoute: FC<Props> = (props) => {
     return (
-        <div>
+        <div className="flex flex-row">
+            <Sidebar></Sidebar>
             <Route path="/dashboard">
                 <DashboardPage></DashboardPage>
             </Route>
